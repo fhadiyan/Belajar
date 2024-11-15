52dd8c20abef4ff07455a3cafd392bb79e6d6d3a
Your Name <fharyhadiyan@gmail.com>
2024-11-15 16:48:15 +0700

    Commit lagi

---
+++ belajar-js/input.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+     
+
+const num1 = prompt('Enter a first number: ');
+
+const num2 = prompt('Enter a second number: ');
+
+ 
+
+console.log('first number + second number =');
+
+console.log(Number(num1) + Number(num2));
+
+
+const prompt = require('prompt-sync')({ sigint: true });
+const num1 = prompt('Enter a first number: '); // Meminta pengguna untuk memasukkan angka pertama const num2 = prompt('Enter a second number: '); // Meminta pengguna untuk memasukkan angka kedua console.log('first number + second number ='); // Menampilkan teks untuk menandakan hasil penjumlahan // Menghitung penjumlahan angka pertama dan angka kedua, kemudian menampilkannya console.log(Number(num1) + Number(num2));
